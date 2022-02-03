6c4bd44b82da7ea4eb3201f2b606d12dae684e85
unknown <malice@korea.ac.kr>
2022-02-04 01:25:11 +0900

    #2 extending the schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,13 @@
+const nicolas = {
+    name: "Nico",
+    age: 19,
+    gender: "male"
+};
+
+
 const resolvers ={
     Query:{
-        name: ()=> "nicolas"
+        person: ()=> nicolas
     }
 }
 
